3d492b7a1a9d06830f1c252155e2776cd38323c0
triumvirsacademia <triumvirsacademia@gmail.com>
2024-11-08 02:20:44 +1100

    stupid vercel

--- app/nav-link.tsx
+++ app/nav-link.tsx
@@ -1,13 +1,11 @@
 'use client'
 
 import { usePathname } from 'next/navigation'
-import Link from 'next/link'
 
 export default function Underline({
   href,
   exact = false,
   className,
-  ...props
 }: {
   href: string
   exact?: boolean
