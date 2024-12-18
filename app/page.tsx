cb01b180d51b607e121e2ebe41f55639ded11bb4
triumvirsacademia <triumvirsacademia@gmail.com>
2024-12-19 01:31:53 +1100

    more mobile hcanges

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,17 @@ export default function Home() {
     return (
         <>
             <div className="relative h-screen">
-                <div className="absolute top-10 left-20 w-[calc(40vw/3)] h-[calc(40vw/3)] 3xl:h-64 3xl:w-64">
+                <div className="absolute top-4 md:top-8 left-10 md:left-20 w-32 h-32 md:w-48 md:h-48 3xl:h-64 3xl:w-64">
                     <Image src="/triumvirs_academia_logo_clear.png" alt="Triumvirs Academia Logo" fill={true} />
                 </div>
                 <div className="absolute flex h-screen w-screen justify-center items-center">
-                    <div>
-                        <div className="flex w-[884px] justify-between h-[10.75rem]">
-                            <div className="relative text-[9rem]  font-bold tracking-tighter">Triumvirs
+                    <div className="hidden md:block">
+                        <div className="flex w-[42rem] lg:w-[884px] justify-between h-[10.75rem]">
+                            <div className="relative text-[9rem] font-bold tracking-tighter">Triumvirs
                                 <span className="absolute -left-10 bottom-[0.75rem] h-[0.0625rem] bg-black w-[42rem]"></span>
                             </div>
                         </div>
-                        <div className="relative flex w-[884px] justify-between h-[10.75rem] items-end">
+                        <div className="relative flex w-[42rem] lg:w-[884px] justify-between h-[10.75rem] items-end">
                             <div></div>
                             <div className="relative">
                                 <div className="text-[9rem] font-bold tracking-tighter">Academia</div>
@@ -27,13 +27,30 @@ export default function Home() {
                             </div>
                         </div>
                     </div>
+                    <div className="block md:hidden">
+                        <div className="flex w-[24rem] justify-between">
+                            <div className="relative text-7xl font-bold tracking-tighter">Triumvirs
+                                <span className="absolute -left-8 bottom-[9px] h-[0.0625rem] bg-black w-96"></span>
+                            </div>
+                        </div>
+                        <div className="relative flex w-[24rem] justify-between items-end h-16">
+                            <div></div>
+                            <div className="relative">
+                                <div className="text-7xl font-bold tracking-tighter">Academia</div>
+                                <span className="absolute -left-8 bottom-[9px] h-[0.0625rem] bg-black w-96"></span>
+                                <span className="absolute left-0 -top-[1px] h-24 bg-black w-[0.0625rem]"></span>
+                                <div className="absolute right-0 bottom-8 h-32 w-32 md:h-64 md:w-64 rounded-full bg-gradient-to-r from-[#737373] to-[#D9D9D9] mix-blend-difference"></div>
+                                <span className="absolute bottom-40 right-16 h-[50vh] bg-black w-[0.0625rem]"></span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="absolute h-fit bottom-[4rem] w-screen flex justify-center">
-                    <div className="w-full max-w-6xl mx-auto px-4 md:px-6 py-24">
+                <div className="absolute h-fit bottom-[10vh] w-screen flex justify-center">
+                    <div className="w-full max-w-6xl mx-auto px-4 md:px-6">
                         <div className="text-center">                            
-                            <div className="font-serif tracking-tighter text-3xl md:text-4xl">A reinvention of  
+                            <div className="pl-[4vw] font-serif tracking-tighter text-3xl md:text-4xl">A reinvention of 
                                 <span className="inline-flex flex-col h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.4xl)*theme(lineHeight.tight))] overflow-hidden">
-                                    <ul className="pl-3 block animate-text-slide text-left leading-tight [&_li]:block">
+                                    <ul className="pl-2 block animate-text-slide text-left leading-tight [&_li]:block">
                                         <li>education</li>
                                         <li>mentorship</li>
                                         <li>communication</li>
