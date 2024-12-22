2eb75d891a91175432161a0e9e63559c60a5e135
triumvirsacademia <triumvirsacademia@gmail.com>
2024-12-22 13:02:29 +1100

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Triumvirs Academia",
@@ -33,6 +34,7 @@ export default function RootLayout({
         {children}
       </body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
